15f38e0daad71d534b5af1405239cd7dff2d705f
Rodolfo Santos Silveira <rodolfo360tisilveira@gmail.com>
2019-07-14 13:27:33 -0300

    construindo dashboard

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { Content } from './styles'
+import { 
+    Content, 
+    Container, 
+    CardArea, 
+    Card, 
+    HomeTitle, 
+    CardTitle,
+    CardValue 
+} from './styles'
 
 function Main() {
     return (
         <Content>
-            <h1>Conteudo</h1>
+            <Container>
+                <HomeTitle>Dashboard</HomeTitle>
+                <CardArea>
+                    <Card>
+                        <CardTitle>Clientes</CardTitle>
+                        <CardValue>0</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Funcionários</CardTitle>
+                        <CardValue>0</CardValue>
+                    </Card>
+                    <Card>
+                        <CardTitle>Chamados</CardTitle>
+                        <CardValue>0</CardValue>
+                    </Card>
+                </CardArea>
+            </Container>
         </Content>
     )
 }
